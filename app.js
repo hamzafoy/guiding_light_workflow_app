c1937c1bc12768e33a9c5ce0fd09634052089076
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2020-10-21 20:37:42 -0400

    add current date per Hijri Calendar to the date display.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let randomAyat = Math.floor((Math.random() * 6235) + 1);
 const url = `http://api.alquran.cloud/v1/ayah/${randomAyat}/editions/quran-uthmani,en.sahih`;
-const zipcode = 40220;
+const zipcode = 40218;
 import { openWeatherAPIKey } from './carKeys.js';
 const url_weather = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=imperial&APPID=${openWeatherAPIKey}`;
 
@@ -43,47 +43,6 @@ function getTime() {
     clockFace.innerHTML = timeOfDay;
 };
 
-function getDate() {
-    let dateDisplay = document.getElementById('currentDateToday');
-    let daysOfTheWeek = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday'
-    ];
-    let months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    ];
-
-    let currentTime = new Date();
-
-    let day = currentTime.getDay();
-    let dayToday = daysOfTheWeek[`${day}`];
-    let month = currentTime.getMonth();
-    let monthToday = months[`${month}`];
-    let dateToday = currentTime.getDate();
-    let yearToday = currentTime.getFullYear();
-
-    let dateMessage = `Today is ${dayToday}, ${monthToday} ${dateToday} ${yearToday}`;
-    dateDisplay.innerHTML = dateMessage;
-}
-
-
 async function getWeather() {
     try {
         let response = await fetch(url_weather);
@@ -96,8 +55,56 @@ async function getWeather() {
     }
 }
 
+async function getHijri() {
+    try {
+        let dateDisplay = document.getElementById('currentDateToday');
+
+        let daysOfTheWeek = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday'
+        ];
+        let months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+
+        let currentTime = new Date();
+        let day = currentTime.getDay();
+        let dayToday = daysOfTheWeek[`${day}`];
+        let month = currentTime.getMonth();
+        let monthToday = months[`${month}`];
+        let dateToday = currentTime.getDate();
+        let yearToday = currentTime.getFullYear();
+        let dateMessage = `Today is ${dayToday}, ${monthToday} ${dateToday} ${yearToday}`;
+        
+        let url_hijri = `http://api.aladhan.com/v1/gToH?date=21-10-2020`
+        let response = await fetch(url_hijri);
+        let hijriDateToday = await response.json();
+        let currentHijri = `${hijriDateToday.data.hijri.month.en} ${hijriDateToday.data.hijri.day}, ${hijriDateToday.data.hijri.year} AH`;
+        dateDisplay.innerHTML = `${dateMessage} </br> ${currentHijri}`;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getHijri();
 getAyat();
 getWeather();
 getTime();
 setInterval(getTime, 1000);
-getDate();
